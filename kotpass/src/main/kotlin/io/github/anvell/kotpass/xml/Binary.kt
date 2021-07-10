64583f65433ba9d4868b087f92ec04d88f8cca0c
Denis Trotsenko <denis.tnko@gmail.com>
2021-07-10 02:35:49 +0200

    Use getBytes helper

--- kotpass/src/main/kotlin/io/github/anvell/kotpass/xml/Binary.kt
+++ kotpass/src/main/kotlin/io/github/anvell/kotpass/xml/Binary.kt
@@ -2,10 +2,9 @@ package io.github.anvell.kotpass.xml
 
 import io.github.anvell.kotpass.errors.FormatError
 import io.github.anvell.kotpass.extensions.addBytes
-import io.github.anvell.kotpass.extensions.getText
+import io.github.anvell.kotpass.extensions.getBytes
 import io.github.anvell.kotpass.models.Binary
 import io.github.anvell.kotpass.models.BinaryData
-import org.apache.commons.codec.binary.Base64.decodeBase64
 import org.redundent.kotlin.xml.Node
 import org.redundent.kotlin.xml.node
 
@@ -15,17 +14,18 @@ import org.redundent.kotlin.xml.node
 internal fun Binary.Companion.unmarshal(node: Node): Binary = with(node) {
     val id = get<String?>(FormatXml.Attributes.Id)?.toInt()
         ?: throw FormatError.InvalidXml("Binary node has no id.")
-    val text = getText()
+    val bytes = getBytes()
         ?: throw FormatError.InvalidXml("Empty body of binary node with id: $id.")
     val compressed = get<String?>(FormatXml.Attributes.Compressed).toBoolean()
-    val binaryData = decodeBase64(text).let {
-        when {
-            compressed -> BinaryData.Compressed(it)
-            else -> BinaryData.Uncompressed(it)
-        }
-    }
 
-    return Binary(id, false, binaryData)
+    return Binary(
+        id = id,
+        memoryProtection = false,
+        data = when {
+            compressed -> BinaryData.Compressed(bytes)
+            else -> BinaryData.Uncompressed(bytes)
+        }
+    )
 }
 
 internal fun Binary.marshal(): Node {
