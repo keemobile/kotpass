e873d4c1736c2b91555d091db94747fc100696f4
Denis Trotsenko <denis.tnko@gmail.com>
2021-07-15 21:43:18 +0200

    Enlarge errors set

--- kotpass/src/main/kotlin/io/github/anvell/kotpass/errors/FormatError.kt
+++ kotpass/src/main/kotlin/io/github/anvell/kotpass/errors/FormatError.kt
@@ -1,6 +1,9 @@
 package io.github.anvell.kotpass.errors
 
 sealed class FormatError : Exception() {
+    class UnknownFormat(override val message: String) : FormatError()
+    class UnsupportedVersion(override val message: String) : FormatError()
+    class InvalidHeader(override val message: String) : FormatError()
     class InvalidXml(override val message: String) : FormatError()
     class FailedCompression(override val message: String) : FormatError()
 }
