aa7b8add0e41b89f1cf9fb8f8045c711807df06e
Denis Trotsenko <denis.tnko@gmail.com>
2021-07-22 17:00:26 +0200

    Added Blake2b support

---
+++ kotpass/src/test/kotlin/io/github/anvell/kotpass/cryptography/Blake2bDigestSpec.kt
@@ -0,0 +1,27 @@
+package io.github.anvell.kotpass.cryptography
+
+import io.github.anvell.kotpass.resources.Blake2bDigestRes
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class Blake2bDigestSpec : DescribeSpec({
+
+    describe("Blake2b digest") {
+        it("Properly hashes input") {
+            Blake2bDigestRes.TestCases.forEach {
+                val output = ByteArray(it.outputLength)
+                Blake2bDigest(
+                    key = it.key,
+                    digestLength = it.outputLength,
+                    salt = it.salt,
+                    personalization = it.personalization
+                ).apply {
+                    update(it.input, 0, it.input.size)
+                    doFinal(output, 0)
+                }
+
+                output shouldBe it.output
+            }
+        }
+    }
+})
