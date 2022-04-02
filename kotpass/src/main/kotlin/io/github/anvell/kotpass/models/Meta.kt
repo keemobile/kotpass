d7fca06945cbbfba2f0263a8c5bbc146f2259845
Denis Trotsenko <denis.tnko@gmail.com>
2022-04-02 14:48:15 +0200

    • Update timestamps when 'modifyMeta' is used • Added KDoc from 'Meta'

--- kotpass/src/main/kotlin/io/github/anvell/kotpass/models/Meta.kt
+++ kotpass/src/main/kotlin/io/github/anvell/kotpass/models/Meta.kt
@@ -6,6 +6,41 @@ import okio.ByteString
 import java.time.Instant
 import java.util.*
 
+/**
+ * Meta describes various database meta data.
+ * Note that *MasterKeyChangeForceOnce* is omitted.
+ *
+ * @property generator Application name that generated database file.
+ * @property headerHash The SHA-256 hash of the database header data (version 3.x).
+ * @property settingsChanged Timestamp of the last change of database settings.
+ * @property name Database name.
+ * @property nameChanged Timestamp of the last change of [name].
+ * @property description Database description.
+ * @property descriptionChanged Timestamp of the last change of [description].
+ * @property defaultUser Username to use as a default when creating a new entry in the database.
+ * @property defaultUserChanged Timestamp of the last change of [defaultUser].
+ * @property maintenanceHistoryDays Indicates the maximum age of the oldest history item
+ * to keep in days when performing database maintenance.
+ * @property color Can be used to show a colored database icon.
+ * @property masterKeyChanged Timestamp of the last change of the database’s master key.
+ * @property masterKeyChangeRec Indicates the number of days after which application should
+ * recommend changing the database’s master key (-1 means never).
+ * @property masterKeyChangeForce Indicates the number of days after which application should
+ * force changing the database’s master key (-1 means never).
+ * @property recycleBinEnabled Indicates whether database should use the recycle bin.
+ * @property recycleBinUuid [UUID] of the recycle bin group inside the database.
+ * @property recycleBinChanged Timestamp of the last change of [recycleBinUuid].
+ * @property entryTemplatesGroup [UUID] of the group that holds templates for the new entries.
+ * @property entryTemplatesGroupChanged Timestamp of the last change of [entryTemplatesGroup].
+ * @property historyMaxItems Maximum number of historic items [Entry] should store.
+ * @property historyMaxSize Indicates maximum history size in bytes.
+ * @property lastSelectedGroup [UUID] of the group that was last selected by the user.
+ * @property lastTopVisibleGroup [UUID] of the top-most group that is visible in the user’s last session.
+ * @property memoryProtection Indicates which fields should be protected by in-memory encryption at runtime.
+ * @property customIcons Collection of attached binary files which could be used as custom icons.
+ * @property customData Used by plugins to store arbitrary string data at database level.
+ * @property binaries Collection of attached binary files (version 3.x).
+ */
 data class Meta(
     val generator: String = Defaults.Generator,
     val headerHash: ByteString? = null,
