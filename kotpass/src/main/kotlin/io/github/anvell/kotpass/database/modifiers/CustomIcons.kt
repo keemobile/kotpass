3bb2a1c8eb4fc2b3b84fca28de66de5825524814
Denis Trotsenko <denis.tnko@gmail.com>
2022-06-19 20:19:14 +0200

    Added 'modifyCustomIcons' helper

---
+++ kotpass/src/main/kotlin/io/github/anvell/kotpass/database/modifiers/CustomIcons.kt
@@ -0,0 +1,26 @@
+package io.github.anvell.kotpass.database.modifiers
+
+import io.github.anvell.kotpass.database.KeePassDatabase
+import io.github.anvell.kotpass.models.CustomIcon
+import java.util.*
+
+/**
+ * Replaces [Map] of [CustomIcon] while removing invalid references in entries.
+ */
+inline fun KeePassDatabase.modifyCustomIcons(
+    crossinline block: (Map<UUID, CustomIcon>) -> Map<UUID, CustomIcon>
+): KeePassDatabase {
+    val newCustomIcons = block(content.meta.customIcons)
+    val removedUuids = content.meta.customIcons.keys
+        .filter { it !in newCustomIcons.keys }
+
+    return modifyEntries {
+        if (customIconUuid in removedUuids) {
+            copy(customIconUuid = null)
+        } else {
+            this
+        }
+    }.modifyMeta {
+        copy(customIcons = newCustomIcons)
+    }
+}
