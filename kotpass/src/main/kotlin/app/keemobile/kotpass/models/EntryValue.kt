674a3668f6b452e61bd116581813f046904d4d0b
Denis Trotsenko <denis.tnko@gmail.com>
2022-11-08 21:46:53 +0100

    • Marked EntryValue cases as data classes • Added kdocs

--- kotpass/src/main/kotlin/app/keemobile/kotpass/models/EntryValue.kt
+++ kotpass/src/main/kotlin/app/keemobile/kotpass/models/EntryValue.kt
@@ -2,19 +2,34 @@ package app.keemobile.kotpass.models
 
 import app.keemobile.kotpass.cryptography.EncryptedValue
 
+/**
+ * Wraps value fields stored in [Entry].
+ */
 sealed class EntryValue {
+    /**
+     * Returns underlying value, decrypting if required.
+     */
     abstract val content: String
 
-    class Plain(
+    /**
+     * Should be used for non-sensitive values.
+     */
+    data class Plain(
         override val content: String
     ) : EntryValue()
 
-    class Encrypted(
+    /**
+     * Should be used for secrets.
+     */
+    data class Encrypted(
         private val value: EncryptedValue
     ) : EntryValue() {
         override val content: String get() = value.text
     }
 
+    /**
+     * Replaces wrapped value with result of the [block].
+     */
     inline fun map(block: (String) -> String) = when (this) {
         is Plain -> Plain(block(content))
         is Encrypted -> Encrypted(EncryptedValue.fromString(block(content)))
