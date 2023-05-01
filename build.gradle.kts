a60fcafa877edd0f3702e9073de0962cf78c0b07
Denis Trotsenko <denis.tnko@gmail.com>
2023-05-01 13:20:47 +0200

    Add `versions` plugin to check dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("maven-publish")
     id("com.diffplug.spotless") version "6.18.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 subprojects {
@@ -15,5 +18,14 @@ subprojects {
             targetExclude("bin/**/*.kt")
             ktlint(libs.versions.ktlint.get())
         }
+    }
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        val version = candidate.version.lowercase()
+
+        listOf("-alpha", "-beta", "-rc")
+            .any { it in version }
     }
 }
