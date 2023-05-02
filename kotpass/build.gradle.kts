3f588616ccd9a20d480e3f934b6c626fa4734612
lukwol <wolanczyk.lukasz@gmail.com>
2023-05-02 09:32:33 +0200

    Migrate to Kotlin Multiplatform

--- kotpass/build.gradle.kts
+++ kotpass/build.gradle.kts
@@ -1,20 +1,39 @@
-@file:Suppress("PropertyName")
+@file:Suppress("PrivatePropertyName")
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ArtifactId = "kotpass"
-val ArtifactGroup = "app.keemobile"
-val ArtifactVersion = "0.4.12"
+private val ArtifactId = "kotpass"
+private val ArtifactGroup = "app.keemobile"
+private val ArtifactVersion = "0.4.12"
 
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.dokka)
-    id("java-library")
     id("maven-publish")
 }
 
 group = ArtifactGroup
 version = ArtifactVersion
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        getByName("jvmMain") {
+            dependencies {
+                implementation(libs.kotlin.xml.builder)
+                implementation(libs.okio)
+            }
+        }
+
+        getByName("jvmTest") {
+            dependencies {
+                implementation(libs.testing.kotest)
+                implementation(libs.kotlin.reflect)
+            }
+        }
+    }
+}
 
 repositories {
     mavenCentral()
@@ -24,52 +43,14 @@ repositories {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         jvmTarget = "1.8"
-        freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
     }
 }
 
 java {
-    withSourcesJar()
-
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks.withType<Jar> {
-    manifest.attributes(
-        mapOf(
-            "Implementation-Title" to project.name,
-            "Implementation-Version" to project.version
-        )
-    )
-}
-
-tasks.create<Jar>("sourceJar") {
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = ArtifactGroup
-            artifactId = ArtifactId
-            version = ArtifactVersion
-
-            from(components["java"])
-            artifact(tasks["sourceJar"])
-        }
-    }
-}
-
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-dependencies {
-    testImplementation(libs.testing.kotest)
-    testImplementation(libs.kotlin.reflect)
-
-    implementation(libs.kotlin.xml.builder)
-    implementation(libs.okio)
 }
