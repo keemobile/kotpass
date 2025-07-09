0a4d266d2c6d1f5ea75e84366d2baeed24a78020
Denis Trotsenko <denis@keemobile.app>
2025-07-09 22:41:03 +0200

    Add CustomIconsSpec

---
+++ kotpass/src/test/kotlin/app/keemobile/kotpass/xml/CustomIconsSpec.kt
@@ -0,0 +1,47 @@
+package app.keemobile.kotpass.xml
+
+import app.keemobile.kotpass.common.renderTestXmlString
+import app.keemobile.kotpass.constants.Const
+import app.keemobile.kotpass.cryptography.EncryptionSaltGenerator
+import app.keemobile.kotpass.models.FormatVersion
+import app.keemobile.kotpass.models.XmlContext
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.ints.shouldNotBeZero
+import io.kotest.matchers.shouldBe
+import okio.ByteString.Companion.toByteString
+import org.redundent.kotlin.xml.parse
+
+class CustomIconsSpec : DescribeSpec({
+
+    describe("CustomIcons XML") {
+        it("Deserialize XML") {
+            val pngSignature = Const.bytes(0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A)
+            val document = ClassLoader
+                .getSystemResourceAsStream("xml/custom_icons.xml")!!
+                .use(::parse)
+            val icons = CustomIcons.unmarshal(document)
+
+            icons.size.shouldNotBeZero()
+
+            val slice = icons.values
+                .first()
+                .data
+                .toByteString(0, pngSignature.size)
+            slice shouldBe pngSignature
+        }
+
+        it("Serialize XML") {
+            val context = XmlContext.Encode(
+                version = FormatVersion(4, 1),
+                encryption = EncryptionSaltGenerator.ChaCha20(byteArrayOf()),
+                binaries = linkedMapOf()
+            )
+            val resourceStream = { ClassLoader.getSystemResourceAsStream("xml/custom_icons.xml")!! }
+            val document = resourceStream().use(::parse)
+            val rawData = resourceStream().readAllBytes().decodeToString()
+            val icons = CustomIcons.unmarshal(document)
+
+            renderTestXmlString(CustomIcons.marshal(context, icons)) shouldBe rawData
+        }
+    }
+})
