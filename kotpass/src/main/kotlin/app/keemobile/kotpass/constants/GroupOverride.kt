0cd97f0ceae706048e03a325440f15a5445b7e85
Denis Trotsenko <denis@keemobile.app>
2025-07-12 18:57:18 +0200

    Add KDocs

--- kotpass/src/main/kotlin/app/keemobile/kotpass/constants/GroupOverride.kt
+++ kotpass/src/main/kotlin/app/keemobile/kotpass/constants/GroupOverride.kt
@@ -1,5 +1,9 @@
 package app.keemobile.kotpass.constants
 
+/**
+ * Specifies whether a feature for a group is inherited from its parent
+ * or is explicitly enabled/disabled.
+ */
 enum class GroupOverride {
     Inherit,
     Enabled,
