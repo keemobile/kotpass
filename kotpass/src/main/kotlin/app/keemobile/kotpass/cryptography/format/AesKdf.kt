8a696b2b071c0c0486036453d7bb3b72675bfdf1
Denis Trotsenko <denis@keemobile.app>
2025-09-28 18:36:30 +0200

    Avoid mutating input key passed to `AesKdf::transformKey`

--- kotpass/src/main/kotlin/app/keemobile/kotpass/cryptography/format/AesKdf.kt
+++ kotpass/src/main/kotlin/app/keemobile/kotpass/cryptography/format/AesKdf.kt
@@ -1,6 +1,7 @@
 package app.keemobile.kotpass.cryptography.format
 
 import app.keemobile.kotpass.errors.CryptoError
+import app.keemobile.kotpass.extensions.clear
 import app.keemobile.kotpass.extensions.sha256
 import java.security.GeneralSecurityException
 import java.security.NoSuchAlgorithmException
@@ -12,21 +13,27 @@ internal object AesKdf {
         key: ByteArray,
         seed: ByteArray,
         rounds: ULong
-    ): ByteArray = try {
-        val cipher = Cipher.getInstance("AES/ECB/NoPadding")
-        val keySpec = SecretKeySpec(seed, "AES")
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+    ): ByteArray {
+        val bytes = key.copyOf()
 
-        repeat(rounds.toInt()) {
-            cipher.update(key, 0, 16, key, 0)
-            cipher.update(key, 16, 16, key, 16)
-        }
-        key.sha256()
-    } catch (error: GeneralSecurityException) {
-        if (error is NoSuchAlgorithmException) {
-            throw CryptoError.AlgorithmUnavailable("AES/ECB encryption is not supported in current environment.")
-        } else {
-            throw CryptoError.InvalidKey("Wrong KDF seed used for decryption.")
+        return try {
+            val cipher = Cipher.getInstance("AES/ECB/NoPadding")
+            val keySpec = SecretKeySpec(seed, "AES")
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec)
+
+            repeat(rounds.toInt()) {
+                cipher.update(bytes, 0, 16, bytes, 0)
+                cipher.update(bytes, 16, 16, bytes, 16)
+            }
+            bytes.sha256()
+        } catch (error: GeneralSecurityException) {
+            if (error is NoSuchAlgorithmException) {
+                throw CryptoError.AlgorithmUnavailable("AES/ECB encryption is not supported in current environment.")
+            } else {
+                throw CryptoError.InvalidKey("Wrong KDF seed used for decryption.")
+            }
+        } finally {
+            bytes.clear()
         }
     }
 }
